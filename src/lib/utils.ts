7f0531968cdd421a1e28d9b6045b32e57ee48aaa
PriscaTonia <priscaebube@gmail.com>
2025-07-31 12:53:15 +0100

    Fixx

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,4 +11,4 @@ export const scaleStyle =
 export const httpPaperlink =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_DOMAIN;
+    : process.env.NEXT_PUBLIC_DOMAIN || "";
